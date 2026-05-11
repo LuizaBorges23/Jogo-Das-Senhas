746b93e7f69cce9591a526ea52dc912a062dd1e6
Maria Luiza <luluborgescordeiro@gmail.com>
2026-05-11 20:40:07 -0300

    Funcionalidades do Jogo na visão do servidor

---
+++ Downloads/Jogo-Das-Senhas-main/Jogo-Das-Senhas-main/src/main/java/JogoInterface.java
@@ -0,0 +1,9 @@
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface JogoInterface extends Remote {
+    void jogar(int tentativa) throws RemoteException;
+    void mostrarjogo() throws RemoteException;
+
+}
---
+++ Downloads/Jogo-Das-Senhas-main/Jogo-Das-Senhas-main/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
+public class Main{
+
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+
+
+    }
+}
+
