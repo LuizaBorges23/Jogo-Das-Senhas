69fc086f159a982e788ddf22523df2112f2e971f
Maria Luiza <157306008+LuizaBorges23@users.noreply.github.com>
2026-05-11 20:50:20 -0300

    Delete Downloads/Jogo-Das-Senhas-main directory

--- Downloads/Jogo-Das-Senhas-main/Jogo-Das-Senhas-main/src/main/java/JogoInterface.java
+++
@@ -1,9 +0,0 @@
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface JogoInterface extends Remote {
-    void jogar(int tentativa) throws RemoteException;
-    void mostrarjogo() throws RemoteException;
-
-}
